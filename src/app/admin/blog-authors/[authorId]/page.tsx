330d6181e3103e5c856f6ac180399b4b8122fdb1
Logica Binaria <logicabinariaconta@gmail.com>
2025-06-16 06:24:26 +0600

    Google Analytics and Edge issue resolved

--- src/app/admin/blog-authors/[authorId]/page.tsx
+++ src/app/admin/blog-authors/[authorId]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const runtime = 'edge';
+
 import { useState, useEffect } from "react";
 import { BlogAuthor } from "../components/author-list";
 import { useParams, useRouter } from "next/navigation";
--- src/app/admin/blogs/[blogId]/page.tsx
+++ src/app/admin/blogs/[blogId]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const runtime = 'edge';
+
 import { useState, useEffect } from "react";
 import { BlogPost } from "../types";
 import { useParams, useRouter } from "next/navigation";
